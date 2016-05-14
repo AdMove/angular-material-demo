f4c84327c01a0aabba00ad00111d091831337577
Giorgi <giorgirokhadze@gmail.com>
2016-05-14 16:46:30 +0400

    Some updates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,32 @@
 (function () {
     'use strict';
 
-    angular.module('app', ['ngMaterial', 'ngMessages']);
+    angular
+        .module('app', ['ngMaterial', 'ngMessages', 'ngRoute'])
+        .config(config);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'templates/home.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'templates/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'templates/register.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({redirectTo: '/login'});
+    }
+
 })();
