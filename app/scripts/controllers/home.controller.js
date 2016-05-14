d72cfc7d279a293eac8e01b8b329aaf9dcc7b8ed
Giorgi <giorgirokhadze@gmail.com>
2016-05-14 19:14:10 +0400

    Created home view

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -3,11 +3,86 @@
 
     angular
         .module('app')
-        .controller('HomeController', RegisterController);
+        .controller('HomeController', HomeController)
+        .controller('LeftCtrl', LeftCtrl)
+        .controller('RightCtrl', RightCtrl);
 
-    RegisterController.$inject = [];
-    function RegisterController() {
+    HomeController.$inject = ['$scope', '$timeout', '$mdSidenav', '$log'];
+    function HomeController($scope, $timeout, $mdSidenav, $log) {
+        $scope.isOpen = false;
 
+        $scope.navigationBar = {
+            isOpen: false,
+            count: 0,
+            selectedDirection: 'right'
+        };
+
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function () {
+            return $mdSidenav('right').isOpen();
+        };
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function () {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function () {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+
+        function buildToggler(navID) {
+            return function () {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }
+        }
     }
 
+    LeftCtrl.$inject = ['$scope', '$timeout', '$mdSidenav', '$log'];
+    function LeftCtrl($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+        };
+    }
+
+    RightCtrl.$inject = ['$scope', '$timeout', '$mdSidenav', '$log'];
+    function RightCtrl($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('right').close()
+                .then(function () {
+                    $log.debug("close RIGHT is done");
+                });
+        };
+    }
+    
 })();
